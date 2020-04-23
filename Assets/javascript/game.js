22d4057d772c178abfe70b321da11e7e4b99b0e2
Jeff Turner <jeffgill57@gmail.com>
2020-04-23 16:26:59 -0500

    compiled some css java

---
+++ Assets/javascript/game.js
@@ -0,0 +1,27 @@
+function Fighter(
+    name,
+    image,
+)
+{
+    this.name=name;
+    this.image=image;
+}
+
+    function initAllFighters(){
+    fighter.push(
+        new Fighter ("Baby Yoda", "./Assets/images/yoda.jpg")
+    );
+    fighter.push (
+        new Fighter ("Jar Jar Binks", "./Assets/images/jarjar.jpg")
+    );
+    fighter.push (
+        new Fighter("Jabba The Hut", "./Assets/images/jaba.jpg")
+    );
+    fighter.push (
+        new Fighter("Rey", "./Assets/images/Rey.jpg")
+    );
+        
+}
+
+
+  
